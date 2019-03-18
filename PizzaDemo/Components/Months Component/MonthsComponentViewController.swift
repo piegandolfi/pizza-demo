dc0b93114739d462226e49ffcf1395ba7f447943
Pietro Gandolfi <piegandolfi@gmail.com>
2019-03-18 18:21:56 +0100

    adding images

---
+++ PizzaDemo/Components/Months Component/MonthsComponentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MonthsComponentViewController.swift
+//  PizzaDemo
+//
+//  Created by Pietro Gandolfi on 18/03/2019.
+//  Copyright © 2019 Pietro Gandolfi. All rights reserved.
+//
+
+import UIKit
+
+class MonthsComponentViewController: UIViewController {
+
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Actions
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
